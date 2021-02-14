162ddc8351ac053353f48fec49f02d330887b524
EC2 Default User <ec2-user@ip-172-31-19-52.ec2.internal>
2021-02-14 03:13:12 +0000

    コントローラー作成

---
+++ app/controllers/aces_controller.rb
@@ -0,0 +1,2 @@
+class AcesController < ApplicationController
+end
---
+++ test/controllers/aces_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AcesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
